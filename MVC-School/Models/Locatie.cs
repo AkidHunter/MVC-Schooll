dcd299e392fd55f401591eeaee5e3b1ed41fa0b9
AkidHunter <70744444+AkidHunter@users.noreply.github.com>
2021-05-10 10:12:54 +0200

    Add project files.

---
+++ MVC-School/Models/Locatie.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVC_School.Models
+{
+    public class Locatie
+    {
+        public int Id { get; set; }
+
+        [StringLength(20)]
+        public string Voornaam { get; set; }
+
+        [StringLength(20)]
+        public string Achternaam { get; set; }
+
+        [StringLength(40)]
+        public string Adres { get; set; }
+
+        [StringLength(40)]
+        public string Woonplaats { get; set; }
+    }
+}
