c567fae6138eface530b774e87fc04975db567c5
AkidHunter <70744444+AkidHunter@users.noreply.github.com>
2021-05-10 11:09:47 +0200

    Opdracht 4 (commit from opdracht 3 missed StudentenController.cs. now added)

---
+++ MVC-School/Models/Docent.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVC_School.Models
+{
+    public class Docent
+    {
+
+        public int Id { get; set; }
+
+        [StringLength(20)]
+        public string Voornaam { get; set; }
+
+        [StringLength(40)]
+        public string Achternaam { get; set; }
+
+        [ForeignKey("Locatie")]
+        public int LocatieId { get; set; }
+
+        public virtual Locatie Locatie { get; set; }
+    }
+}
--- MVC-School/Models/Locatie.cs
+++ MVC-School/Models/Locatie.cs
@@ -8,10 +8,15 @@ namespace MVC_School.Models
 {
     public class Locatie
     {
+        public Locatie()
+        {
+            Docenten = new HashSet<Docent>();
+        }
+
         public int Id { get; set; }
 
         [StringLength(20)]
-        public string Voornaam { get; set; }
+        public string Naam { get; set; }
 
         [StringLength(20)]
         public string Achternaam { get; set; }
@@ -21,5 +26,7 @@ namespace MVC_School.Models
 
         [StringLength(40)]
         public string Woonplaats { get; set; }
+
+        public ICollection<Docent> Docenten { get; set; }
     }
 }
